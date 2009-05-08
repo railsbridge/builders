5b8c353283d07395bf73ff6f24ccc21e2381ec40
Mike Breen <hardbap@gmail.com>
2009-05-07 22:31:19 -0400

    unpack action_mailer_tls gem

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.gem 'authlogic'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem "openrain-action_mailer_tls", :lib => "smtp_tls.rb", :source => "http://gems.github.com"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
