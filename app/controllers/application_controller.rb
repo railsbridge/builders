9e306b59c6244a5ed1c2bcc2fb2bd1fcc4696925
Mike Breen <hardbap@gmail.com>
2009-06-04 16:23:40 -0400

    remove ApplicationController#require_no_user. re: yagni.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,15 +28,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def require_no_user
-    if current_user
-      store_location
-      flash[:notice] = "You must be logged out to access this page"
-      redirect_to users_url
-      return false
-    end
-  end
-  
   def store_location
     session[:return_to] = request.request_uri
   end
