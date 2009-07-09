97790a71f9c261e43ea599c5c1d51234c4c4ce85
Mike Breen <hardbap@gmail.com>
2009-07-09 11:15:52 -0400

    set the RR version to 0.9.0 because 0.10.0 breaks how we're stubbing ActionMailer.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,4 +29,4 @@ config.action_mailer.delivery_method = :test
 config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :source => 'http://gems.github.com'
 config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
 config.gem 'webrat'
-config.gem 'rr'
+config.gem 'rr', :version => '<=0.9.0'
