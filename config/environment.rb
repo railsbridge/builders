71f7dccd138c36b17cf5a3d2517b3f3402d74f18
Mike Breen <hardbap@gmail.com>
2009-10-28 15:26:22 -0400

    Upgrade to Rails 2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
 
   config.gem 'chrislloyd-gravtastic',   :lib => 'gravtastic',    :source => 'http://gems.github.com'
 
-  config.gem 'pluginaweek-state_machine', 
+  config.gem 'pluginaweek-state_machine',
     :lib     => 'state_machine',
     :version => '0.7.6',
     :source  => 'http://gems.github.com'
